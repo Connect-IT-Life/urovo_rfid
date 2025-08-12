01bf70d3eecbc0d7c2a6dd22d1708b7ec760dd28
Devii Naicker <devii@naicker.tech>
2025-08-12 14:59:16 +0200

    feat: add RFID platform channel

--- test/urovo_rfid_test.dart
+++ test/urovo_rfid_test.dart
@@ -10,6 +10,43 @@ class MockUrovoRfidPlatform
 
   @override
   Future<String?> getPlatformVersion() => Future.value('42');
+
+  @override
+  Stream<Map<String, dynamic>> get inventoryStream => const Stream.empty();
+
+  @override
+  Future<bool?> init() async => true;
+
+  @override
+  Future<void> startInventory(int session) async {}
+
+  @override
+  Future<void> stopInventory() async {}
+
+  @override
+  Future<Map<String, dynamic>?> readTag(
+          String epc, int memBank, int wordAdd, int wordCnt, List<int> password) async =>
+      {};
+
+  @override
+  Future<int?> writeTag(String epc, List<int> password, int memBank,
+          int wordAdd, List<int> data) async =>
+      0;
+
+  @override
+  Future<int?> writeTagEpc(String epc, String password, String data) async => 0;
+
+  @override
+  Future<int?> getOutputPower() async => 0;
+
+  @override
+  Future<int?> setOutputPower(int power) async => 0;
+
+  @override
+  Future<void> setInventoryParameter(Map<String, dynamic> params) async {}
+
+  @override
+  Future<void> enableScanHead(bool enable) async {}
 }
 
 void main() {
